fa73c743b4cda52ecc17fe8c2f8079cada5bedca
Manny Modrono <21243@columbushs.com>
2019-11-18 11:15:12 -0500

    started 4

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -18,13 +18,25 @@ function orderByYear(movieList) {
 
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct
 
-function howManyMovies(movieList){
-    const movieList.filter((movieList) =>)
+function howManyMovies(movieList) {
+    let newList = movieList.filter(movies => movies.director === 'Steven Spielberg');
+    return newList.filter(movies => movies.genre.includes('Drama')).length;
 }
 
 // Iteration 3: Alphabetic Order - Order by title and print the first 20 titles
 
+function orderAlphabetically(movieList) {
+    let moveiTitles = movieList.map(movie => movie.title);
+    let sortedList = moveiTitles.sort();
+    return sortedList.slice(0, 20);
+}
+
 // Iteration 4: All rates average - Get the average of all rates with 2 decimals
+
+function ratesAverage(movieList){
+    let movieRates = movieList.map(movie => movie.rate);
+    return movieRates.reduce(a, b => a += b) / movieRates.length;
+}
 
 // Iteration 5: Drama movies - Get the average of Drama Movies
 
